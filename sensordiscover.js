a9aeab4859d5479d856695c49557314baf20a502
Michael Koert <mkoert@bluebikeproductions.com>
2016-10-21 22:07:32 -0400

    johnny five alive

---
+++ sensordiscover.js
@@ -0,0 +1,13 @@
+var SensorTag = require('sensortag');
+
+function onDiscover(sensorTag) {
+  console.log('discovered: ' + sensorTag.id + ', type = ' + sensorTag.type);
+}
+
+// SensorTag.discoverAll(onDiscover);
+
+SensorTag.discoverById('20d1a47d7bab4a89aa2b0d3cbb8372f5', onDiscover);
+
+// SensorTag.discoverByAddress('90:59:af:0a:ab:34', onDiscover);
+
+SensorTag.discover(onDiscover);
